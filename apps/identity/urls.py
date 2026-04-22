4acd710dadcb32273dcf79139b961cc39f2e274b
Nasor Hidar <130475719+NasorHidar@users.noreply.github.com>
2026-04-22 10:55:12 +0600

    Delete apps/identity directory

--- apps/identity/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('vault/', views.identity_vault, name='identity_vault'),
-    path('vault/success/', views.vault_success, name='vault_success'), # Added
-    path('vault/deny/', views.vault_deny, name='vault_deny'),          # Added
-]
